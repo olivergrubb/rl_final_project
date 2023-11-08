5dce9c28bd32e694808bc5a81d6d94fd29e4324d
Oliver Grubb <45010380+olivergrubb@users.noreply.github.com>
2023-11-08 16:02:20 +0000

    Add files via upload

---
+++ rl_methods/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+package_name = 'rl_methods'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='ollie',
+    maintainer_email='ollie@todo.todo',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'talker = rl_methods.colour_detection_node:main'
+        ],
+    },
+)
